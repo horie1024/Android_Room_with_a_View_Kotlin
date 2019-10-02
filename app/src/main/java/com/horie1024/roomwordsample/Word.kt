1c6125c5417fe2e9cb6be5db8ee3e48d26cbdb74
horie1024 <ryosuke.pic@gmail.com>
2019-10-02 11:41:43 +0900

    5.Create the DAO

--- app/src/main/java/com/horie1024/roomwordsample/Word.kt
+++ app/src/main/java/com/horie1024/roomwordsample/Word.kt
@@ -1,3 +1,8 @@
 package com.horie1024.roomwordsample
 
-data class Word(val word: String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String, val a:String)
